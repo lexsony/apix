51c9c121d672481340043ac97b67ed30d9d72c2f
yonzkon <yonzkon@gmail.com>
2023-02-16 15:46:21 +0800

    feat(apix-cli): save msg before print

--- demos/apix-cli.c
+++ demos/apix-cli.c
@@ -16,8 +16,11 @@
 #include <apix-posix.h>
 #include <srrp.h>
 #include <log.h>
+#include <atbuf.h>
 #include "opt.h"
 #include "cli.h"
+
+#define KBYTES 1024 * 1024
 
 static int exit_flag;
 static struct apix *ctx;
@@ -26,11 +29,13 @@ struct fd_struct {
     int fd;
     char addr[64];
     const char *mode;
+    atbuf_t *msg;
 };
 
 static struct fd_struct fds[1024];
 static const char *cur_mode = "#";
 static int cur_fd = -1;
+static int print_all = 0;
 
 static void signal_handler(int sig)
 {
@@ -45,6 +50,18 @@ static struct opt opttab[] = {
 
 static int client_pollin(int fd, const char *buf, size_t len)
 {
+    if (fds[fd].msg == NULL) {
+        fds[fd].msg = atbuf_new(KBYTES);
+    }
+    if (atbuf_spare(fds[fd].msg) < len)
+        atbuf_clear(fds[fd].msg);
+    atbuf_write(fds[fd].msg, buf, len);
+
+    return len;
+}
+
+static void print_cur_msg(void)
+{
     int need_hack = (rl_readline_state & RL_STATE_READCMD) > 0;
     char *saved_line;
     int saved_point;
@@ -57,7 +74,19 @@ static int client_pollin(int fd, const char *buf, size_t len)
         rl_redisplay();
     }
 
-    printf("%s", buf);
+    if (cur_fd != -1 && fds[cur_fd].msg && atbuf_used(fds[cur_fd].msg)) {
+        printf("[From %s]:\n", fds[cur_fd].addr);
+        char msg[256] = {0};
+        size_t len = 0;
+        while (1) {
+            bzero(msg, sizeof(msg));
+            len = atbuf_read(fds[cur_fd].msg, msg, sizeof(msg));
+            if (len == 0) break;
+            printf("%s", msg);
+        }
+        printf("\n---------------------------\n");
+        atbuf_clear(fds[cur_fd].msg);
+    }
 
     if (need_hack) {
         rl_restore_prompt();
@@ -66,8 +95,45 @@ static int client_pollin(int fd, const char *buf, size_t len)
         rl_redisplay();
         free(saved_line);
     }
+}
 
-    return len;
+static void print_all_msg(void)
+{
+    int need_hack = (rl_readline_state & RL_STATE_READCMD) > 0;
+    char *saved_line;
+    int saved_point;
+
+    if (need_hack) {
+        saved_point = rl_point;
+        saved_line = rl_copy_text(0, rl_end);
+        rl_save_prompt();
+        rl_replace_line("", 0);
+        rl_redisplay();
+    }
+
+    for (int i = 0; i < sizeof(fds) / sizeof(fds[0]); i++) {
+        if (fds[i].msg && atbuf_used(fds[i].msg)) {
+            printf("[From %s]:\n", fds[i].addr);
+            char msg[256] = {0};
+            size_t len = 0;
+            while (1) {
+                bzero(msg, sizeof(msg));
+                len = atbuf_read(fds[i].msg, msg, sizeof(msg));
+                if (len == 0) break;
+                printf("%s", msg);
+            }
+            printf("\n---------------------------\n");
+            atbuf_clear(fds[i].msg);
+        }
+    }
+
+    if (need_hack) {
+        rl_restore_prompt();
+        rl_replace_line(saved_line, 0);
+        rl_point = saved_point;
+        rl_redisplay();
+        free(saved_line);
+    }
 }
 
 static void *apix_thread(void *arg)
@@ -76,6 +142,10 @@ static void *apix_thread(void *arg)
     apix_enable_posix(ctx);
 
     while (exit_flag == 0) {
+        if (print_all)
+            print_all_msg();
+        else
+            print_cur_msg();
         apix_poll(ctx);
     }
 
@@ -97,6 +167,18 @@ static void on_cmd_exit(const char *cmd)
     } else {
         cur_mode = "#";
         cur_fd = -1;
+    }
+}
+
+static void on_cmd_print(const char *cmd)
+{
+    char param[64] = {0};
+    int nr = sscanf(cmd, "print %s", param);
+    if (nr == 1) {
+        if (strcmp(param, "all") == 0)
+            print_all = 1;
+        else if (strcmp(param, "cur") == 0)
+            print_all = 0;
     }
 }
 
@@ -236,6 +318,10 @@ static void on_cmd_close(const char *cmd)
                 cur_fd = -1;
             apix_close(ctx, fd);
             fds[fd].fd = 0;
+            if (fds[fd].msg) {
+                atbuf_delete(fds[fd].msg);
+                fds[fd].msg = NULL;
+            }
         }
     }
 }
@@ -264,6 +350,7 @@ static const struct cli_cmd cli_cmds[] = {
     { "!", on_cmd_history_exec, "!<num>" },
     { "quit", on_cmd_quit, "quit cli" },
     { "exit", on_cmd_exit, "exit cur_mode or quit cli" },
+    { "print", on_cmd_print, "print all|cur" },
     { "ll", on_cmd_fds, "list fds" },
     { "fds", on_cmd_fds, "list fds" },
     { "use", on_cmd_use, "set frontend fd" },
